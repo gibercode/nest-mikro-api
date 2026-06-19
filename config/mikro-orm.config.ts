91d5f91a868c96c1a12b400c6eb22d5d1aa21ba4
gibercode <giberlara@gmail.com>
2026-06-18 20:14:23 -0400

    auto load models

--- config/mikro-orm.config.ts
+++ config/mikro-orm.config.ts
@@ -1,16 +1,15 @@
 import 'dotenv/config';
-import { defineConfig } from '@mikro-orm/postgresql';
 import { ReflectMetadataProvider } from '@mikro-orm/decorators/legacy';
+import { MikroOrmModuleOptions } from '@mikro-orm/nestjs';
 
-const ormCOnfig = defineConfig({
+const ormCOnfig: MikroOrmModuleOptions = {
   metadataProvider: ReflectMetadataProvider,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432', 10),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   dbName: process.env.DB_NAME,
-  entities: ['./dist/**/*.entity.js'],
-  entitiesTs: ['./src/**/*.entity.ts'],
+  autoLoadEntities: true,
   migrations: {
     path: './src/db/migrations',
     pathTs: './src/db/migrations',
@@ -23,6 +22,6 @@ const ormCOnfig = defineConfig({
     fileName: (className: string) => className,
   },
   debug: process.env.NODE_ENV !== 'production',
-});
+};
 
 export default ormCOnfig;
